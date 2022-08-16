4b3b45f47583d198360a92462c153185afd2905c
David Clark <kc0osh@gmail.com>
2022-08-16 18:30:11 -0500

    Completed the 2nd deliverable.

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -32,3 +32,24 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title 
 ORDER BY count DESC;
+
+
+-- Deliverable #2
+-- write a query to create a Mentorship Eligibility table that holds the
+-- employees who are eligible to participate in a mentorship program
+SELECT DISTINCT ON (e.emp_no) t.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date, 
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+	ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+	ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') 
+	AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Pewlett-Hackard-Analysis
+# Pewlett Hackard Analysis
+
+## Overview
+  * Purpose of analysis is well defined.
+
+## Results of Analysis
+  * Bulleted List with four major points from the two analysis deliverables.
+
+## Summary
+  * Summary addresses the two questions and contains two additional queries or tables that may provide more insight.
